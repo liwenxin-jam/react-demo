6b73005fb90f37e53e00feba002f62a50d82e30e
liwenxin-jam <mxlwx@126.com>
2020-03-28 18:22:38 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React from 'react'
+import React from "react";
+import Routes from "./Routes";
 
 function App() {
-  return (
-    <div className="App">
-      app
-    </div>
-  )
+  return <Routes />;
 }
 
-export default App
+export default App;
