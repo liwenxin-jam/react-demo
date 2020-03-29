8340ce6c977da2a3827af223ed3fae2ff469b4de
liwenxin-jam <mxlwx@126.com>
2020-03-29 19:14:24 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import UseReducerPage from "./pages/UseReducerPage";
+import UseContextPage from "./pages/UseContextPage";
+import { MyContext } from "./AppContext";
+import UseRefPage from "./pages/UseRefPage";
 
 function App() {
   return (
-    <div className="App">
-      app
+    <div className='App'>
+      {/* <MyContext.Provider value={{themeColor: "red"}}>
+        <UseContextPage />
+      </MyContext.Provider> */}
+
+      {/* <UseReducerPage /> */}
+      <UseRefPage />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
